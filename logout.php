d58bcb4cb34e75d57272db2608ceabc50501faf8
e-tskhe <youngartist7009@gmail.com>
2025-06-03 08:23:58 +0300

    Add files via upload

--- logout.php
+++ logout.php
@@ -4,7 +4,9 @@ header("X-Content-Type-Options: nosniff");
 header("X-XSS-Protection: 1; mode=block");
 header("Referrer-Policy: no-referrer-when-downgrade");
 
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 session_unset();
 session_destroy();
 header("Location: index.html");
