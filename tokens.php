31536aedabca6010709ec0e2026c230678c89386
e-tskhe <youngartist7009@gmail.com>
2025-06-03 09:07:39 +0300

    Update tokens.php

--- tokens.php
+++ tokens.php
@@ -1,8 +1,8 @@
 <?php
 function generateCSRFToken() {
-    if (session_status() === PHP_SESSION_NONE) {
-        session_start();
-    }
+    // if (session_status() === PHP_SESSION_NONE) {
+    //     session_start();
+    // }
 
     if (empty($_SESSION['csrf_token'])) {
         $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
